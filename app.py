9d624ef972cca096f5d28ff6fd70a32c3d1260db
dataResearchLab <132771578+dataResearchLab@users.noreply.github.com>
2023-05-09 14:13:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,34 @@
-from dash import Dash, dcc, html, Input, Output
-from raceplotly.plots import barplot
-import  pandas as pd
-from data import  *
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
 app = Dash(__name__)
-app.layout = html.Div([
-    html.H2('Data Comparison over Years'),
-    html.P("Select an option:"),
-    dcc.RadioItems(
-        id='selection',
-        options=["GDP", "Population"],
-        value='GDP',
-    ),
-    dcc.Loading(dcc.Graph(id="graph"), type="cube")
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
 ])
 
-
-@app.callback(
-    Output("graph", "figure"),
-    Input("selection", "value"))
-def display_animated_graph(selection):
-    if selection == 'GDP':
-        data=get_gdp()
-    else:
-        data=get_pop()
-    my_raceplot = barplot(data, item_column='name', value_column='value', time_column='year',)
-    graph=my_raceplot.plot(item_label='Top Group', value_label='value', frame_duration=500)
-    return graph
-
-app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
